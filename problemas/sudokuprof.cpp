5b540306ecbe8c530c4aaf2ccc06b7a5dc784c42
Quimaerical <singingseba@gmail.com>
2022-10-22 17:19:10 -0400

    all

--- problemas/sudokuprof.cpp
+++ problemas/sudokuprof.cpp
@@ -19,12 +19,12 @@ for Sudoku solution (non-duplication across rows, columns, and boxes) */
 bool SolveSudoku(int grid[N][N]){  
  int row, col;  
  // If there is no unassigned location, we are done  
- if (!FindUnassignedLocation(grid, row, col))  
- return true; // success!  
+ if (!FindUnassignedLocation(grid, row, col))
+ return true; // success! 
  
  // consider digits 1 to 9  
  for (int num = 1; num <= 9; num++)  
- {  
+ {
   // if looks promising  
   if (isSafe(grid, row, col, num))  
   {  
@@ -56,7 +56,7 @@ bool FindUnassignedLocation(int grid[N][N], int &row, int &col){
  
 /* Returns a boolean which indicates whether an assigned entry  
 in the specified row matches the given number. */ 
-bool UsedInRow(int grid[N][N], int row, int num)  {  
+bool UsedInRow(int grid[N][N], int row, int num){  
  for (int col = 0; col < N; col++)  
   if (grid[row][col] == num)  
    return true;  
@@ -64,7 +64,7 @@ bool UsedInRow(int grid[N][N], int row, int num)  {
 }  
  /* Returns a boolean which indicates whether an assigned entry  
 in the specified column matches the given number. */ 
-bool UsedInCol(int grid[N][N], int col, int num)  {  
+bool UsedInCol(int grid[N][N], int col, int num){  
  for (int row = 0; row < N; row++)  
   if (grid[row][col] == num)  
    return true;  
@@ -73,7 +73,7 @@ bool UsedInCol(int grid[N][N], int col, int num)  {
  
 /* Returns a boolean which indicates whether an assigned entry  
 within the specified 3x3 box matches the given number. */ 
-bool UsedInBox(int grid[N][N], int boxStartRow, int boxStartCol, int num)  {  
+bool UsedInBox(int grid[N][N], int boxStartRow, int boxStartCol, int num){  
  for (int row = 0; row < 3; row++)  
   for (int col = 0; col < 3; col++)  
    if (grid[row+boxStartRow][col+boxStartCol] == num)  
@@ -83,7 +83,7 @@ bool UsedInBox(int grid[N][N], int boxStartRow, int boxStartCol, int num)  {
  
 /* Returns a boolean which indicates whether it will be legal to assign  
 num to the given row,col location. */ 
-bool isSafe(int grid[N][N], int row, int col, int num)  {  
+bool isSafe(int grid[N][N], int row, int col, int num){  
  /* Check if 'num' is not already placed in current row,  
  current column and current 3x3 box */ 
  return !UsedInRow(grid, row, num) &&  
@@ -93,7 +93,7 @@ bool isSafe(int grid[N][N], int row, int col, int num)  {
 }  
  
 /* A utility function to print grid */ 
-void printGrid(int grid[N][N])  {  
+void printGrid(int grid[N][N]){  
  for (int row = 0; row < N; row++)  
  {  
  for (int col = 0; col < N; col++)  
@@ -103,10 +103,11 @@ void printGrid(int grid[N][N])  {
 }  
  
 /* Driver Program to test above functions */ 
-int main()  {  
+int main(){  
  // 0 means unassigned cells 
- int grid[N][N] = {{3, 0, 6, 5, 0, 8, 4, 0, 0},  
-     {5, 2, 0, 0, 0, 0, 0, 0, 0},  
+ int grid[N][N] = {
+	 {3, 0, 6, 5, 0, 8, 4, 0, 0},  
+	 {5, 2, 0, 0, 0, 0, 0, 0, 0},  
      {0, 8, 7, 0, 0, 0, 0, 3, 1},  
      {0, 0, 3, 0, 1, 0, 0, 8, 0},  
      {9, 0, 0, 8, 6, 3, 0, 0, 5},  
